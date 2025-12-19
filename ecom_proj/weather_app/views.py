a4df5a64c931f69da4cfbb8e152047b1da03ccb2
HCRICKET <heinhaung91@gmail.com>
2025-12-19 01:33:06 -0500

    midnigt

--- ecom_proj/weather_app/views.py
+++ ecom_proj/weather_app/views.py
@@ -3,10 +3,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.conf import settings
+from rest_framework.permissions import AllowAny
+
 
 class WeatherView(APIView):
+    permission_classes =[AllowAny]
     def post(self, request):
-        city = request.data.get("city", "Fayetteville")
+        city = request.data.get("city")
         api_key = settings.WEATHER_API_KEY
 
         url = "https://api.weatherapi.com/v1/current.json"
